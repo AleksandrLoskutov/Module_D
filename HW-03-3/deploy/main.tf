7596ff9a323f1e1db5cab9a9d9b0499e5172a765
AleksandrLoskutov <107686705+AleksandrLoskutov@users.noreply.github.com>
2022-10-19 18:11:43 +0300

    Create main.tf

---
+++ HW-03-3/deploy/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~>0.65.0"
+    }
+  }
+}
+provider "yandex" {
+  token     = "AQAAAABil4pGAATuwSm3yuphu0CCuHJ7Uq6pfNE"
+  cloud_id  = "b1gt4a9dmrcir454d9hr"
+  folder_id = "b1grvvp5v2u8mpqo1vcl"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "swarm-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.100.0/24"]
+}
+
+module "swarm_cluster" {
+  source        = "./modules/instance"
+  vpc_subnet_id = yandex_vpc_subnet.subnet.id
+  managers      = 1
+  workers       = 2
+}
